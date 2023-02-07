8ea656add7184b21df3849f5478269a33c0a8a3f
mango mac <1092017732@qq.com>
2023-02-07 16:21:27 +0800

    发布1.0.0版本

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # docsify-giscus
-docsify giscus plugin
+docsify giscus plugin，集成giscus评论插件！
+
+[![npm](https://img.shields.io/npm/v/docsify-plugin-toc.svg?style=flat-square)](https://www.npmjs.com/package/docsify-giscus)
+
+
+## Usage
+1. [Applay giscus](https://giscus.app/zh-CN) ，申请 giscus
+
+2. Configure docsify-giscus:(配置)
+
+    ```html
+    <script>
+    window.$docsify = {
+      giscus:{
+        repo: "mg0324/docsify-note-01",
+        repoId: "R_kgDOI4bELQ",
+        category: "Announcements",
+        categoryId: "DIC_kwDOI4bELc4CT_Su",
+        mapping: "url",
+        reactionsEnabled: "0",
+        strict: "1",
+        emitMetadata: "0",
+        inputPosition: "bottom",
+        theme: "light",
+        lang: "zh-CN",
+        loading: "lazy"
+      },
+    }
+    </script>
+    ```
+
+3. Insert script into docsify document:
+
+    ```html
+    <!-- giscus评论  -->
+    <link rel="stylesheet" href="https://unpkg.com/docsify-giscus@1.0.0/dist/giscus.css">
+    <script src="https://unpkg.com/docsify-giscus@1.0.0/dist/docsify-giscus.min.js"></script>
+    ```
--- src/giscus.js
+++ src/giscus.js
@@ -6,22 +6,23 @@ function renderGiscus(config){
     }
     var giscus = document.createElement("script");
     giscus.src = "https://giscus.app/client.js";
-    giscus.dataset["repo"]=config.repo;//"mg0324/docsify-note-01";
-    giscus.dataset["repoId"]=config.repoId;//"R_kgDOI4bELQ";
-    giscus.dataset["category"]=config.category;//"Announcements";
-    giscus.dataset["categoryId"]=config.categoryId;//"DIC_kwDOI4bELc4CT_Su";
-    giscus.dataset["mapping"]=config.mapping;//"url";
-    giscus.dataset["reactionsEnabled"]=config.reactionsEnabled;//"0";
-    giscus.dataset["strict"]=config.strict;//"1";
-    giscus.dataset["emitMetadata"]=config.emitMetadata;//"0";
-    giscus.dataset["inputPosition"]=config.inputPosition;//"bottom";
-    giscus.dataset["theme"]=config.theme;//"light";
-    giscus.dataset["lang"]=config.lang;//"zh-CN";
-    giscus.dataset["loading"]=config.loading;//"lazy";
-    giscus.crossorigin="anonymous";
+    giscus.dataset["repo"] = config.repo;//"mg0324/docsify-note-01";
+    giscus.dataset["repoId"] = config.repoId;//"R_kgDOI4bELQ";
+    giscus.dataset["category"] = config.category;//"Announcements";
+    giscus.dataset["categoryId"] = config.categoryId;//"DIC_kwDOI4bELc4CT_Su";
+    giscus.dataset["mapping"] = config.mapping;//"url";
+    giscus.dataset["reactionsEnabled"] = config.reactionsEnabled;//"0";
+    giscus.dataset["strict"] = config.strict;//"1";
+    giscus.dataset["emitMetadata"] = config.emitMetadata;//"0";
+    giscus.dataset["inputPosition"] = config.inputPosition;//"bottom";
+    giscus.dataset["theme"] = config.theme;//"light";
+    giscus.dataset["lang"] = config.lang;//"zh-CN";
+    giscus.dataset["loading"] = config.loading;//"lazy";
+    giscus.crossorigin = "anonymous";
     giscus.id = "giscus";
     var s = document.getElementsByTagName("script")[0]; 
     s.parentNode.insertBefore(giscus, s);
+    console.info("[docsify-giscus] rendering!")
 }
 
 export function install (hook, vm) {
